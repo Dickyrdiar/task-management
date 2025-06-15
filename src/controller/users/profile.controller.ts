4f44e24f9bc5590b1b218a0d2dd19ed6a085eebc
dicky ardiar <dickyardiar1@gmail.com>
2025-06-15 17:46:22 +0700

    try to create serverles

--- src/controller/users/profile.controller.ts
+++ src/controller/users/profile.controller.ts
@@ -5,13 +5,40 @@ const prisma = new PrismaClient()
 
 export const findMe = async (req: Request, res: Response): Promise<void> => {
   try {
-    if (!req.user) {
-      res.status(401).json({
-        success: false,
-        message: 'User is not found'
+    const { userId } = req.params;
+    const findUsers = await prisma.user.findUnique({
+      where: { id: userId },
+      include: {
+        ownedProjects: true,
+        projectMemberShip: {
+          include: {
+            user: true
+          }
+        },
+        tickets: {
+          select: {
+            id: true,
+            title: true,
+            status: true,
+            priority: true
+          }
+        }
+      }
+    })
+
+    if (findUsers) {
+      res.status(404).json({
+        message: 'user is not found'
       })
-    
     }
+
+    res.status(200).json({
+      success: true,
+      data: { 
+        findUsers
+      }
+    })
+
   } catch (err) {
     res.status(500).json({
       message: err,

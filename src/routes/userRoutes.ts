dadf3974c51cc2c3738a5d193b97591eabc8fe62
dicky ardiar <dickyardiar1@gmail.com>
2025-06-21 20:01:14 +0700

    feat: adding refresh token

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { createUsers, findAllUsers, findUsersById } from "../controller/users/users.controller";
 import { authMiddleware } from "../middleware/auth.middleware";
+import { RefreshTokn } from "../controller/users/auth.controller";
 
 const router = Router();
 
 router.get('/', authMiddleware, findAllUsers)
 router.post('/', createUsers)
 router.get('/:id', authMiddleware, findUsersById)
+router.post('/refresh-toke', RefreshTokn)
 
 export default router;

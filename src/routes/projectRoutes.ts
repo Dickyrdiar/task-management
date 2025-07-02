4183a34947c6d4c0756b3057ce424054821734f4
dicky ardiar <dickyardiar1@gmail.com>
2025-07-02 22:53:53 +0700

    create agile system in task manager app

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { addUserToProject, createProject, findAllProject, findProjectById } from "../controller/projects/project.controller";
+import { agileSystemById, CreateAgile, findAllSprint } from '../controller/agile/agile.controller'
 
 const router = Router()
 
@@ -7,5 +8,10 @@ router.get('/', findAllProject)
 router.post('/', createProject)
 router.get('/:id', findProjectById)
 router.post('/:projectId/members', addUserToProject)
+
+// agile routes
+router.get('/:projectId/agiles', findAllSprint)
+router.post('/:projectId/agiles', CreateAgile)
+router.get('/:projectId/agiles/:id', agileSystemById)
 
 export default router
